375f2109dcecc8f7feac63bf689aad1bb88d3c70
LuckyShuii <lucasnimes30000@gmail.com>
2024-06-08 21:56:42 +0200

    various css fixes

--- front/src/components/ActivityCard.vue
+++ front/src/components/ActivityCard.vue
@@ -9,12 +9,11 @@ const props = defineProps<{
 
 <template>
     <div class="w-[306px] bg-green-light rounded-[20px] flex flex-col">
-        <img :src="activity.pictures" alt="house" class="w-full h-[250px] object-cover rounded-t" />
-        <div class="flex items-center justify-between px-[16px] py-[14px]">
-            <div class="flex flex-col">
-                <h3 class="font-medium">{{ activity.headerTitle }}</h3>
-            </div>
-            <Button class="w-[110px] h-[30px] text-[14px] flex justify-center">Visiter</Button>
-        </div>
+      <img :src="activity.pictures" alt="house" class="w-full h-[250px] object-cover rounded-t" />
+      <div class="flex items-center justify-between px-[16px] py-[14px] flex-1">
+        <h3 class="font-medium">{{ activity.headerTitle }}</h3>
+        <Button class="w-[110px] h-[30px] text-[14px] flex justify-center">Visiter</Button>
+      </div>
     </div>
-</template>
+  </template>
+  
