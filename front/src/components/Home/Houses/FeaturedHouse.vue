83ae48f57d66e47ece2fb5520446e9d735c397bc
LuckyShuii <lucasnimes30000@gmail.com>
2024-06-08 15:56:46 +0200

    tiny css change

--- front/src/components/Home/Houses/FeaturedHouse.vue
+++ front/src/components/Home/Houses/FeaturedHouse.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineProps, onMounted, ref } from 'vue';
+import { defineProps } from 'vue';
 import type { HouseType } from '@/types/HouseType';
 
 const props = defineProps<{
