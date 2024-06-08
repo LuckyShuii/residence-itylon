9e168b61f23bc99a25bcc92dd143aa0b24d05878
LuckyShuii <lucasnimes30000@gmail.com>
2024-06-08 23:23:28 +0200

    last section pictures

--- front/src/components/Home/LastSection/Pictures.vue
+++ front/src/components/Home/LastSection/Pictures.vue
@@ -5,21 +5,21 @@
     <div class="container max-w-[860px]">
         <div class="grid grid-cols-12 gap-4">
             <div class="col-span-4 h-[225px] bg-gray-200 rounded-lg overflow-hidden">
-                <img src="" alt="Image 1" class="w-full h-full object-cover">
+                <img src="@/assets/photos/home/image1.jpg" alt="Image 1" class="w-full h-full object-cover">
             </div>
             <div class="col-span-4 h-[225px] bg-gray-200 rounded-lg overflow-hidden">
-                <img src="" alt="Image 2" class="w-full h-full object-cover">
+                <img src="@/assets/photos/home/image2.jpg" alt="Image 2" class="w-full h-full object-cover">
             </div>
             <div class="col-span-4 h-[225px] bg-gray-200 rounded-lg overflow-hidden">
-                <img src="" alt="Image 3" class="w-full h-full object-cover">
+                <img src="@/assets/photos/home/image3.jpg" alt="Image 3" class="w-full h-full object-cover">
             </div>
         </div>
         <div class="grid grid-cols-12 gap-4 mt-4">
             <div class="col-span-7 h-[270px] bg-gray-200 rounded-lg overflow-hidden">
-                <img src="" alt="Image 4" class="w-full h-full object-cover">
+                <img src="@/assets/photos/home/image4.jpg" alt="Image 4" class="w-full h-full object-cover">
             </div>
             <div class="col-span-5 h-[270px] bg-gray-200 rounded-lg overflow-hidden">
-                <img src="" alt="Image 5" class="w-full h-full object-cover">
+                <img src="@/assets/photos/home/image5.jpg" alt="Image 5" class="w-full h-full object-cover">
             </div>
         </div>
     </div>
