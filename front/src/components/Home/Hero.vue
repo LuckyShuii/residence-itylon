d54a467aeab8c76452d8282ba390d1991f3d1583
LuckyShuii <lucasnimes30000@gmail.com>
2024-06-08 14:16:28 +0200

    smoother video loading

--- front/src/components/Home/Hero.vue
+++ front/src/components/Home/Hero.vue
@@ -1,12 +1,9 @@
-<script setup lang="ts">
-</script>
-
 <template>
-    <div class="relative h-screen overflow-hidden">
+    <div id="hero-container" class="relative h-screen overflow-hidden opacity-0 transition-opacity duration-500">
         <video class="absolute top-0 left-0 z-0 w-full h-full object-cover" autoplay muted loop>
             <source src="https://www.residence-itylon.com/media/videos/Vid%C3%A9o%20sans%20titre%20%E2%80%93%20R%C3%A9alis%C3%A9e%20avec%20Clipchamp%20(1).mp4" type="video/mp4">
         </video>
-        <div id="hero-content" class="relative z-10 flex items-center flex-col justify-center h-full text-center transition-opacity duration-500 opacity-0">
+        <div class="relative z-10 flex items-center flex-col justify-center h-full text-center">
             <img src="@/assets/logo/logo-hero.svg" alt="logo" class="mx-auto" />
             <Button class="mt-4 w-[172px] h-[46px] flex justify-center">Découvrir</Button>
         </div>
@@ -18,12 +15,12 @@
 import { onMounted } from 'vue';
 
 onMounted(() => {
-    const heroContent = document.getElementById('hero-content');
+    const heroContainer = document.getElementById('hero-container');
     setTimeout(() => {
-        if (heroContent) {
-            heroContent.classList.remove('opacity-0');
-            heroContent.classList.add('opacity-100');
+        if (heroContainer) {
+            heroContainer.classList.remove('opacity-0');
+            heroContainer.classList.add('opacity-100');
         }
-    }, 500);
+    }, 100); // Temps d'attente pour l'affichage initial
 });
 </script>
