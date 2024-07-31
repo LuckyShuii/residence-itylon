f8968f54478a78859ca37c72e5782959bad8c04a
LuckyShuii <lucasnimes30000@gmail.com>
2024-07-31 15:58:57 +0200

    refact checking

--- front/src/components/Contact/ContactForm.vue
+++ front/src/components/Contact/ContactForm.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { reactive } from 'vue';
 
 interface FormData {
   firstName: string;
@@ -9,28 +9,17 @@ interface FormData {
   message: string;
 }
 
-const generateFormData = () => {
-  return {
-    firstName: firstName.value,
-    lastName: lastName.value,
-    email: email.value,
-    phone: phone.value,
-    message: messageText.value,
-  }
-}
-
-const firstName = ref<string>('')
-const lastName = ref<string>('')
-const email = ref<string>('')
-const phone = ref<string>('')
-const messageText = ref<string>('')
-const formData = ref<FormData>(generateFormData())
+const formData = reactive<FormData>({
+  firstName: '',
+  lastName: '',
+  email: '',
+  phone: '',
+  message: ''
+});
 
 const checkForm = (): void => {
-  formData.value = generateFormData()
-  console.log(formData.value)
-  if (Object.entries(formData.value).some(([key, value]) => value === '')) {
-    console.log("Found an empty input")
+  if (Object.values(formData).some(value => value === '')) {
+    console.log("Found an empty input");
   }
 }
 </script>
@@ -42,25 +31,27 @@ const checkForm = (): void => {
       <div class="flex justify-between w-full mb-[2rem]">
         <div class="flex flex-col gap-2">
           <label for="lastName">Nom</label>
-          <InputText id="lastName" v-model="lastName" class="w-[262px] h-[46px] py-[5px] px-[8px] text-[16px] border border-green-btn" />
+          <InputText id="lastName" v-model="formData.lastName" class="w-[262px] h-[46px] py-[5px] px-[8px] text-[16px] border border-green-btn" />
         </div>
         <div class="flex flex-col gap-2">
           <label for="firstName">Prénom</label>
-          <InputText id="firstName" v-model="firstName" class="w-[262px] h-[46px] py-[5px] px-[8px] text-[16px] border border-green-btn" />
+          <InputText id="firstName" v-model="formData.firstName" class="w-[262px] h-[46px] py-[5px] px-[8px] text-[16px] border border-green-btn" />
         </div>
       </div>
       <div class="flex justify-between w-full mb-[2rem]">
         <div class="flex flex-col gap-2">
           <label for="email">Email</label>
-          <InputText id="email" v-model="email" class="w-[262px] h-[46px] py-[5px] px-[8px] text-[16px] border border-green-btn" />
+          <InputText id="email" v-model="formData.email" class="w-[262px] h-[46px] py-[5px] px-[8px] text-[16px] border border-green-btn" />
         </div>
         <div class="flex flex-col gap-2">
           <label for="phone">Téléphone</label>
-          <InputText type="tel" id="phone" v-model="phone" placeholder="0606060606" fluid class="w-[262px] h-[46px] py-[5px] px-[8px] text-[16px] border border-green-btn" />
+          <InputText type="tel" id="phone" v-model="formData.phone" placeholder="0606060606" class="w-[262px] h-[46px] py-[5px] px-[8px] text-[16px] border border-green-btn" />
         </div>
       </div>
-      {{messageText}}
-      <Textarea v-model="messageText" rows="5" cols="30" class="w-full border border-gray-200 rounded-[10px] w-full h-[310px] py-[5px] px-[8px] text-[16px] border border-green-btn" />
+      <div>
+        <label for="message">Message</label>
+        <textarea id="message" v-model="formData.message" rows="5" cols="30" class="w-full border border-gray-200 rounded-[10px] h-[310px] py-[5px] px-[8px] text-[16px] border border-green-btn"></textarea>
+      </div>
 
       <Button type="submit" label="Envoyer" class="mt-[25px] py-[5px] w-[160px]" />
     </form>
