dd88f7147c155f7cfbd7e1d15bb8cf265cc7aba5
LuckyShuii <lucasnimes30000@gmail.com>
2024-08-03 15:19:53 +0200

    added smooth animation when changing activity content

--- front/src/views/ActivitiesView.vue
+++ front/src/views/ActivitiesView.vue
@@ -1,31 +1,32 @@
 <script setup lang="ts">
-import {ref, onMounted, reactive, watch} from 'vue';
+import { ref, onMounted, reactive, watch, nextTick } from 'vue';
 import axios from 'axios';
 import { useToast } from 'primevue/usetoast';
-import { useRoute } from 'vue-router'
+import { useRoute } from 'vue-router';
 import AllActivities from "@/components/Activities/AllActivities.vue";
 import TopImageBlock from "@/components/layout/TopImageBlock.vue";
 import Activity from "@/components/Activities/Activity.vue";
 import type { ActivityType } from '@/types/ActivityType';
-import type { BlockTemplateType } from '@/types/BlockTemplateType'
+import type { BlockTemplateType } from '@/types/BlockTemplateType';
 
-const route = useRoute()
+const route = useRoute();
 
 const toast = useToast();
 const activities = ref<ActivityType[]>([]);
 const activityPictures = ref<string[]>([]);
+const activityContainer = ref<HTMLElement | null>(null);
 
 interface ActivityDetail {
-  title: string,
-  description: string,
-  externalLink: string
+  title: string;
+  description: string;
+  externalLink: string;
 }
 
 const activityDetailData = reactive<ActivityDetail>({
   title: '',
   description: '',
   externalLink: ''
-})
+});
 
 const blockData = reactive<BlockTemplateType>({
   title: '',
@@ -42,14 +43,14 @@ const loadActivities = async () => {
 };
 
 const generateBasicBlockData = () => {
-  blockData.title = "Les activités"
-  blockData.paragraph = "Voluptate vitae quo maxime asperiores. Mollitia iusto qui nemo deserunt magni aut reiciendis consequuntur. Accusantium sunt ducimus assumenda. Praesentium est laborum numquam repellendus"
-  blockData.imageSrc = "../src/assets/photos/activities/plage.jpg"
-}
+  blockData.title = "Les activités";
+  blockData.paragraph = "Voluptate vitae quo maxime asperiores. Mollitia iusto qui nemo deserunt magni aut reiciendis consequuntur. Accusantium sunt ducimus assumenda. Praesentium est laborum numquam repellendus";
+  blockData.imageSrc = "../src/assets/photos/activities/plage.jpg";
+};
 
 const generateImgSrc = (img: string): string => {
-  return '../' + img
-}
+  return '../' + img;
+};
 
 const generateActivityPictures = (images: string): void => {
   activityPictures.value = images.split(',');
@@ -60,43 +61,63 @@ const generateActivityPictures = (images: string): void => {
     }
     return '';
   }).filter((element) => element !== '');
-}
-
+};
 
 const generateAdvancedData = (id: number): void => {
-  let currentActivity = activities.value[id - 1]
-  blockData.title = currentActivity.headerTitle
-  blockData.paragraph = currentActivity.headerDescription
-  blockData.imageSrc = generateImgSrc(currentActivity.pictures.split(",")[0])
-  activityDetailData.title = currentActivity.title
-  activityDetailData.description = currentActivity.description
-  generateActivityPictures(currentActivity.pictures)
-}
+  let currentActivity = activities.value[id - 1];
+  blockData.title = currentActivity.headerTitle;
+  blockData.paragraph = currentActivity.headerDescription;
+  blockData.imageSrc = generateImgSrc(currentActivity.pictures.split(",")[0]);
+  activityDetailData.title = currentActivity.title;
+  activityDetailData.description = currentActivity.description;
+  generateActivityPictures(currentActivity.pictures);
+};
 
 const loadBlockData = (id: number): void => {
-  if (id === 0) generateBasicBlockData()
-  if (id > 0) generateAdvancedData(id)
-}
+  if (id === 0) generateBasicBlockData();
+  if (id > 0) generateAdvancedData(id);
+};
 
-const isAllActivities = () => route.query.a === '0'
+const isAllActivities = () => route.query.a === '0';
 
-watch(async () => route.query, async () => {
-  if (activities.value.length === 0) await loadActivities();
-  let routerValue = route.query.a
-  if (routerValue && typeof routerValue === 'string') loadBlockData(parseInt(routerValue))
-}, { immediate: true })
+watch(
+    () => route.query,
+    async () => {
+      if (activities.value.length === 0) await loadActivities();
+
+      if (activityContainer.value) {
+        activityContainer.value.classList.remove('opacity-100');
+        activityContainer.value.classList.add('opacity-0');
+      }
+
+      setTimeout(async () => {
+        let routerValue = route.query.a;
+        if (routerValue && typeof routerValue === 'string') {
+          loadBlockData(parseInt(routerValue));
+        }
+        await nextTick();
+
+        setTimeout(() => {
+          if (activityContainer.value) {
+            activityContainer.value.classList.remove('opacity-0');
+            activityContainer.value.classList.add('opacity-100');
+          }
+        }, 250);
+      }, 300);
+    },
+    { immediate: true }
+);
 
 onMounted(async () => {
   await loadActivities();
-  console.log(activities.value)
 });
 </script>
 
 <template>
   <TopImageBlock :title="blockData.title" :paragraph="blockData.paragraph" :image-src="blockData.imageSrc" />
-  <AllActivities :activities :isAllActivityView="true" />
+  <AllActivities :activities="activities" :isAllActivityView="true" />
   <hr class="border-0 h-[1px] w-full border-t-2 block mt-[-1rem]" v-if="!isAllActivities()">
-  <div class="w-full flex justify-center items-center" v-if="!isAllActivities()">
+  <div ref="activityContainer" class="w-full flex justify-center items-center opacity-0 transition-opacity duration-500" v-if="!isAllActivities()">
     <Activity :pictures="activityPictures" :title="activityDetailData.title" :description="activityDetailData.description" :external-link="activityDetailData.externalLink" />
   </div>
 </template>
