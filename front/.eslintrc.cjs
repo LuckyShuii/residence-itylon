8c9143b767a569aac3722db1728e20dd19ffdfb9
Lucas Boillot <lucasboillot@mac-1.home>
2025-04-10 20:29:15 +0200

    improved eslint for vue components

--- front/.eslintrc.cjs
+++ front/.eslintrc.cjs
@@ -6,8 +6,13 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-typescript'
+    '@vue/eslint-config-typescript',
   ],
+  rules: {
+    'vue/multi-word-component-names': ['error', {
+      'ignores': ['default']
+    }],
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   }
