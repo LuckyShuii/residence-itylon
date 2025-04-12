0d4b5cd8efee7995518a4a933a41d3b92356a815
LuckyShuii <lucasnimes30000@gmail.com>
2025-04-12 13:42:01 +0200

    global reduces font size in mobile + icon sized better in housepage

--- front/src/components/Houses/HousePage/TopSectionInfo.vue
+++ front/src/components/Houses/HousePage/TopSectionInfo.vue
@@ -21,55 +21,117 @@ const getFormattedTitle = (title?: string): string => {
 <template>
     <div class="mt-[20px] max-[1380px]:flex-col max-[1380px]:px-6 max-[1380px]:w-full">
         <div class="max-w-[380px] h-full rounded-[10px] bg-green-light py-[45px] px-[35px] max-[1380px]:max-w-full">
-            <h3 class="font-mistress text-[40px] pb-[30px]">Découvrez {{ getFormattedTitle(houseData?.title) }}</h3>
+            <h3 class="font-mistress text-[40px] pb-[30px]">
+                Découvrez {{ getFormattedTitle(houseData?.title) }}
+            </h3>
             <p>{{ houseData?.description }}.</p>
         </div>
 
         <div class="w-[500px] rounded-[10px] py-[45px] px-[35px] m-[30px] border border-[#89B58C] max-[1380px]:max-w-full max-[1380px]:w-full max-[1380px]:mx-0">
-            <h3 class="font-mistress text-[40px] pb-[30px] max-[1380px]:text-center">Caractéristiques</h3>
+            <h3 class="font-mistress text-[40px] pb-[30px] max-[1380px]:text-center">
+                Caractéristiques
+            </h3>
             <div class="flex justify-between max-[1380px]:justify-center">
                 <div class="max-[1380px]:mr-8">
                     <div class="flex items-center my-[16px]">
-                        <img loading="lazy" src="/src/assets/icons/double_bed.svg" alt="bed icon" class="w-[30px] h-[30px] inline-block" />
-                        <p class="ml-[20px]">{{ houseData?.bed }} lit{{ Number(houseData?.bed) > 1 ? 's' : '' }}</p>
+                        <img
+                            loading="lazy"
+                            src="/src/assets/icons/double_bed.svg"
+                            alt="bed icon"
+                            class="w-[30px] h-[30px] inline-block max-[700px]:w-[25px] max-[700px]:h-[25px]"
+                        />
+                        <p class="ml-[20px]">
+                            {{ houseData?.bed }} lit{{ Number(houseData?.bed) > 1 ? 's' : '' }}
+                        </p>
                     </div>
                     <div class="flex items-center my-[16px]" v-if="houseData && Number(houseData?.kitchen) > 0">
-                        <img loading="lazy" src="/src/assets/icons/fridge.svg" alt="kitchen icon" class="w-[30px] h-[30px] inline-block" />
+                        <img
+                            loading="lazy"
+                            src="/src/assets/icons/fridge.svg"
+                            alt="kitchen icon"
+                            class="w-[30px] h-[30px] inline-block max-[700px]:w-[25px] max-[700px]:h-[25px]"
+                        />
                         <p class="ml-[20px]">Cuisine équipée</p>
                     </div>
                     <div class="flex items-center my-[16px]" v-if="houseData && Number(houseData?.wifi) > 0">
-                        <img loading="lazy" src="/src/assets/icons/wifi.svg" alt="wifi icon" class="w-[30px] h-[30px] inline-block" />
+                        <img
+                            loading="lazy"
+                            src="/src/assets/icons/wifi.svg"
+                            alt="wifi icon"
+                            class="w-[30px] h-[30px] inline-block max-[700px]:w-[25px] max-[700px]:h-[25px]"
+                        />
                         <p class="ml-[20px]">Wifi gratuit</p>
                     </div>
                     <div class="flex items-center my-[16px]" v-if="houseData && Number(houseData?.ac) > 0">
-                        <img loading="lazy" src="/src/assets/icons/ac.svg" alt="ac icon" class="w-[30px] h-[30px] inline-block" />
+                        <img
+                            loading="lazy"
+                            src="/src/assets/icons/ac.svg"
+                            alt="ac icon"
+                            class="w-[30px] h-[30px] inline-block max-[700px]:w-[25px] max-[700px]:h-[25px]"
+                        />
                         <p class="ml-[20px]">Climatisation</p>
                     </div>
                     <div class="flex items-center my-[16px]" v-if="houseData && Number(houseData?.room) > 0">
-                        <img loading="lazy" src="/src/assets/icons/room.svg" alt="room icon" class="w-[30px] h-[30px] inline-block" />
-                        <p class="ml-[20px]">{{ houseData?.room }} Chambre{{ Number(houseData?.room) > 1 ? 's' : '' }}</p>
+                        <img
+                            loading="lazy"
+                            src="/src/assets/icons/room.svg"
+                            alt="room icon"
+                            class="w-[30px] h-[30px] inline-block max-[700px]:w-[25px] max-[700px]:h-[25px]"
+                        />
+                        <p class="ml-[20px]">
+                            {{ houseData?.room }} Chambre{{ Number(houseData?.room) > 1 ? 's' : '' }}
+                        </p>
                     </div>
                 </div>
                 <div>
                     <div class="flex items-center my-[16px]" v-if="houseData && Number(houseData?.terrace) > 0">
-                        <img loading="lazy" src="/src/assets/icons/terrace.svg" alt="terrace icon" class="w-[30px] h-[30px] inline-block" />
+                        <img
+                            loading="lazy"
+                            src="/src/assets/icons/terrace.svg"
+                            alt="terrace icon"
+                            class="w-[30px] h-[30px] inline-block max-[700px]:w-[25px] max-[700px]:h-[25px]"
+                        />
                         <p class="ml-[20px]">Terrasse</p>
                     </div>
                     <div class="flex items-center my-[16px]" v-if="houseData && Number(houseData?.barbecue) > 0">
-                        <img loading="lazy" src="/src/assets/icons/bbq.svg" alt="wifi icon" class="w-[30px] h-[30px] inline-block" />
+                        <img
+                            loading="lazy"
+                            src="/src/assets/icons/bbq.svg"
+                            alt="wifi icon"
+                            class="w-[30px] h-[30px] inline-block max-[700px]:w-[25px] max-[700px]:h-[25px]"
+                        />
                         <p class="ml-[20px]">Barbecue</p>
                     </div>
                     <div class="flex items-center my-[16px]" v-if="houseData && Number(houseData?.parking) > 0">
-                        <img loading="lazy" src="/src/assets/icons/parking.svg" alt="parking icon" class="w-[30px] h-[30px] inline-block" />
+                        <img
+                            loading="lazy"
+                            src="/src/assets/icons/parking.svg"
+                            alt="parking icon"
+                            class="w-[30px] h-[30px] inline-block max-[700px]:w-[25px] max-[700px]:h-[25px]"
+                        />
                         <p class="ml-[20px]">Place de parking</p>
                     </div>
                     <div class="flex items-center my-[16px]" v-if="houseData && Number(houseData?.bathroom) > 0">
-                        <img loading="lazy" src="/src/assets/icons/bathroom.svg" alt="bathroom icon" class="w-[30px] h-[30px] inline-block" />
-                        <p class="ml-[20px]">{{ houseData?.bathroom }} Salle de bain{{ Number(houseData?.bathroom) > 1 ? 's' : '' }}</p>
+                        <img
+                            loading="lazy"
+                            src="/src/assets/icons/bathroom.svg"
+                            alt="bathroom icon"
+                            class="w-[30px] h-[30px] inline-block max-[700px]:w-[25px] max-[700px]:h-[25px]"
+                        />
+                        <p class="ml-[20px]">
+                            {{ houseData?.bathroom }} Salle de bain{{ Number(houseData?.bathroom) > 1 ? 's' : '' }}
+                        </p>
                     </div>
                     <div class="flex items-center my-[16px]" v-if="houseData && Number(houseData?.canapeLit) > 0">
-                        <img loading="lazy" src="/src/assets/icons/sofa.svg" alt="sofa icon" class="w-[30px] h-[30px] inline-block" />
-                        <p class="ml-[20px]">{{ houseData?.canapeLit }} Canapé{{ Number(houseData?.canapeLit) > 1 ? 's' : '' }}-lit{{ Number(houseData?.canapeLit) > 1 ? 's' : '' }}</p>
+                        <img
+                            loading="lazy"
+                            src="/src/assets/icons/sofa.svg"
+                            alt="sofa icon"
+                            class="w-[30px] h-[30px] inline-block max-[700px]:w-[25px] max-[700px]:h-[25px]"
+                        />
+                        <p class="ml-[20px]">
+                            {{ houseData?.canapeLit }} Canapé{{ Number(houseData?.canapeLit) > 1 ? 's' : '' }}-lit{{ Number(houseData?.canapeLit) > 1 ? 's' : '' }}
+                        </p>
                     </div>
                 </div>
             </div>
@@ -82,23 +144,33 @@ const getFormattedTitle = (title?: string): string => {
                 <tbody>
                     <tr>
                         <td>Avril, Mai & Octobre</td>
-                        <td class="max-[1380px]:text-end">{{ houseData?.price[0].lowSeason }}€</td>
+                        <td class="max-[1380px]:text-end">
+                            {{ houseData?.price[0].lowSeason }}€
+                        </td>
                     </tr>
                     <tr>
                         <td>Juin & Septembre</td>
-                        <td class="max-[1380px]:text-end">{{ houseData?.price[0].mediumSeason }}€</td>
+                        <td class="max-[1380px]:text-end">
+                            {{ houseData?.price[0].mediumSeason }}€
+                        </td>
                     </tr>
                     <tr>
                         <td>01/07 - 27/07</td>
-                        <td class="max-[1380px]:text-end">{{ houseData?.price[0].highSeason }}€</td>
+                        <td class="max-[1380px]:text-end">
+                            {{ houseData?.price[0].highSeason }}€
+                        </td>
                     </tr>
                     <tr>
                         <td>28/07 - 24/08</td>
-                        <td class="max-[1380px]:text-end">{{ houseData?.price[0].veryHighSeason }}€</td>
+                        <td class="max-[1380px]:text-end">
+                            {{ houseData?.price[0].veryHighSeason }}€
+                        </td>
                     </tr>
                     <tr>
                         <td>Ménage</td>
-                        <td class="max-[1380px]:text-end">{{ houseData?.price[0].cleaningFee }}€</td>
+                        <td class="max-[1380px]:text-end">
+                            {{ houseData?.price[0].cleaningFee }}€
+                        </td>
                     </tr>
                 </tbody>
             </table>
