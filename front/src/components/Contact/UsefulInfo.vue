00f8c0a25ebd17358244dca078f67effe3ca5ae3
LuckyShuii <lucasnimes30000@gmail.com>
2025-04-12 20:12:57 +0200

    contact link clickables

--- front/src/components/Contact/UsefulInfo.vue
+++ front/src/components/Contact/UsefulInfo.vue
@@ -8,17 +8,17 @@ const { residences } = storeToRefs(useResidenceStore())
 <template>
   <div class="w-full max-w-[545px] min-[1450px]:mt-[80px] min-[1450px]:ml-[100px] mb-[70px]">
     <h2 class="font-mistress text-[46px] m-0">Informations pratiques</h2>
-    <div class="pl-[25px] flex items-center mt-[35px] max-w-[545px] min-h-[65px] bg-green-light rounded-[10px] mb-[30px]">
+    <a class="hover:font-bold" href="https://www.google.com/maps/place/R%C3%A9sidence+Itylon/@42.1363965,8.6169634,17z/data=!3m1!4b1!4m6!3m5!1s0x12da7a32f6062971:0x168704a8aad44050!8m2!3d42.1363926!4d8.6195383!16s%2Fg%2F1tdv3d7d?entry=ttu&g_ep=EgoyMDI1MDQwOS4wIKXMDSoJLDEwMjExNDUzSAFQAw%3D%3D" target="_blank"><div class="pl-[25px] flex items-center mt-[35px] max-w-[545px] min-h-[65px] bg-green-light rounded-[10px] mb-[30px]">
       <img loading="lazy" src="@/assets/icons/home.svg" alt="home" class="w-[20px] mr-[23px]">
-      <p class="m-O">{{residences.postalAddress}}</p>
-    </div>
-    <div class="pl-[25px] flex items-center max-w-[545px] min-h-[65px] bg-green-light rounded-[10px] mb-[30px]">
+      <p class="m-O">{{ residences.postalAddress }}</p>
+    </div></a>
+    <a class="hover:font-bold" :href="`mailto:${residences.email}`"><div class="pl-[25px] flex items-center max-w-[545px] min-h-[65px] bg-green-light rounded-[10px] mb-[30px]">
       <img loading="lazy" src="@/assets/icons/mail.svg" alt="home" class="w-[20px] mr-[23px]">
       <p class="m-O">{{residences.email}}</p>
-    </div>
-    <div class="pl-[25px] flex items-center max-w-[545px] min-h-[65px] bg-green-light rounded-[10px]">
+    </div></a>
+    <a class="hover:font-bold" :href="`tel:${residences.phone}`"><div class="pl-[25px] flex items-center max-w-[545px] min-h-[65px] bg-green-light rounded-[10px]">
       <img loading="lazy" src="@/assets/icons/phone.svg" alt="home" class="w-[20px] mr-[23px]">
       <p class="m-O">{{residences.phone}}</p>
-    </div>
+    </div></a>
   </div>
 </template>
--- front/src/components/Footer/JoinUs.vue
+++ front/src/components/Footer/JoinUs.vue
@@ -11,15 +11,15 @@ const { residences  } = storeToRefs(useResidenceStore());
             <h4 class="text-[16px] font-semibold mb-[15px]">Nous joindre</h4>
             <li class="flex">
                 <img loading="lazy" src="@/assets/icons/home.svg" alt="home">
-                <p class="text-[16px] ml-[13px] !text-center">{{ residences.postalAddress }}</p>
+                <p class="text-[16px] ml-[13px] !text-center hover:border-b-[1px] border-[black]"><a href="https://www.google.com/maps/place/R%C3%A9sidence+Itylon/@42.1363965,8.6169634,17z/data=!3m1!4b1!4m6!3m5!1s0x12da7a32f6062971:0x168704a8aad44050!8m2!3d42.1363926!4d8.6195383!16s%2Fg%2F1tdv3d7d?entry=ttu&g_ep=EgoyMDI1MDQwOS4wIKXMDSoJLDEwMjExNDUzSAFQAw%3D%3D" target="_blank">{{ residences.postalAddress }}</a></p>
             </li>
             <li class="flex">
                 <img loading="lazy" src="@/assets/icons/phone.svg" alt="phone">
-                <p class="text-[16px] ml-[13px]">{{ residences.phone }}</p>
+                <p class="text-[16px] ml-[13px] hover:border-b-[1px] border-[black]"><a :href="`tel:${residences.phone}`">{{ residences.phone }}</a></p>
             </li>
             <li class="flex">
                 <img loading="lazy" src="@/assets/icons/mail.svg" alt="mail">
-                <p class="text-[16px] ml-[13px]">{{ residences.email }}</p>
+                <p class="text-[16px] ml-[13px] hover:border-b-[1px] border-[black]"><a :href="`mailto:${residences.email}`">{{ residences.email }}</a></p>
             </li>
         </div>
     </div>
